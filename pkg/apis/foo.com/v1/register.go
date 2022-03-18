13d936e3ecbbe2ea158e7369e0a62a293a888700
AmrFarid <a.eleraky@eclecticiq.com>
2022-03-18 23:55:32 +0100

    fix register function

--- pkg/apis/foo.com/v1/register.go
+++ pkg/apis/foo.com/v1/register.go
@@ -6,47 +6,22 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// Define your schema name and the version
-var SchemeGroupVersion = schema.GroupVersion{
-	Group:   "foo.com",
-	Version: "v1",
-}
+const GroupName = "foo.com"
+const GroupVersion = "v1"
+
+var SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
 
 var (
-	SchemeBuilder      runtime.SchemeBuilder
-	localSchemeBuilder = &SchemeBuilder
-	AddToScheme        = localSchemeBuilder.AddToScheme
+	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
-func init() {
-	// We only register manually written functions here. The registration of the
-	// generated functions takes place in the generated files. The separation
-	// makes the code compile even when the generated files are missing.
-	localSchemeBuilder.Register(addKnownTypes)
-}
-
-// Resource takes an unqualified resource and returns a Group qualified GroupResource
-func Resource(resource string) schema.GroupResource {
-	return SchemeGroupVersion.WithResource(resource).GroupResource()
-}
-
-// Adds the list of known types to the given scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(
-		SchemeGroupVersion,
+	scheme.AddKnownTypes(SchemeGroupVersion,
 		&HelloType{},
 		&HelloTypeList{},
 	)
 
-	scheme.AddKnownTypes(
-		SchemeGroupVersion,
-		&metav1.Status{},
-	)
-
-	metav1.AddToGroupVersion(
-		scheme,
-		SchemeGroupVersion,
-	)
-
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
 }
